e1e08b5613379e0671428f2d0f06b2166649d6ae
Albauti Lab <albaullab@Tinas-MacBook-Pro.local>
2019-11-01 07:10:00 -0500

    trying another way to make listview widget

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -2,28 +2,45 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePagesTemps extends StatelessWidget {
+  final opciones = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Componenet Temps"),
       ),
-      body: ListView(
+      body: ListView(children: _crearItemCorta()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemCorta() {
+    var widgets = opciones.map((item) {
+      return Column(
         children: <Widget>[
           ListTile(
-            title: Text('ListTile Tile'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Tile'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Tile'),
+            title: Text(item + "!"),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
           ),
           Divider(),
         ],
-      ),
-    );
+      );
+    }).toList();
+    return widgets;
   }
 }
